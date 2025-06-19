2431070855511a21d7d6581766072fdbc852d17d
EduardoMolina18 <em8789743@gmail.com>
2025-06-19 17:10:27 -0600

    Se realiza la conexion a la base de datos de MongoDB

---
+++ app/config/conexion.js
@@ -0,0 +1,16 @@
+const config = require( './configuracion' );
+const mongoose = require ( 'mongoose' );
+
+module.exports={
+    connection: null,
+    connect: () => {
+        if (this.connection) return this.connection;
+        return mongoose.connect(config.DB)
+        .then(conn => {
+            this.connection = conn;
+            console.log('Conectado a la base de datos'); 
+            }
+        )
+        .catch(e => {console.log(`Error en la conexión a la base de datos: ${e}`)}); 
+    }
+}
--- server.js
+++ server.js
@@ -1,5 +1,8 @@
 const CONFIG = require ('./app/config/configuracion');
 const app = require('./app/app');
+const conexión = require('./app/config/conexion');
+
+conexión.connect()
 
 app.listen(CONFIG.PORT, ()=>{
     console.log(`Aplicacion corriendo en puerto, ${CONFIG.PORT}`);
