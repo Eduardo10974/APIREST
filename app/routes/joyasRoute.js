571876f73718ce9593e5219dd7496fa83e430582
EduardoMolina18 <em8789743@gmail.com>
2025-07-03 17:26:02 -0600

    Se realizo el primer endpoint

--- app/routes/joyasRoute.js
+++ app/routes/joyasRoute.js
@@ -1,11 +1,9 @@
 const express = require('express');
 const router = express.Router();
+const joyasController = require('../controllers/joyasController')
 
-router.get('/anillo', (req,res) =>{
-    res.json({
-        mensaje: 'Hola Mundo'
-    })
-} );
+
+router.get('/',joyasController.buscarTodo)
 
 
 module.exports = router;
